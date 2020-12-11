faad4fcd6a6105193018b02aae86631314bae8f4
Dustin <dustinkaban@gmail.com>
2020-12-11 11:13:41 -0800

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpPractice
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public bool OnVacation { get; set; }
+    }
+}
